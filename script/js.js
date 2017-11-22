9fd1a2c4dcf4a54df73754e304b8c47b74e82df2
lqc91 <1007669205@qq.com>
2017-11-22 22:20:44 +0800

    1st modification: Separation of structure and behavior

--- script/js.js
+++ script/js.js
@@ -3,10 +3,11 @@ var sign = calculator.querySelector('.sign');
 var formerInput = calculator.querySelector('.formerInput');
 var laterInput = calculator.querySelector('.laterInput');
 var resultOutput = calculator.querySelector('.resultOutput');
-console.log(sign);
-console.log(formerInput);
-console.log(laterInput);
-console.log(resultOutput);
+var btn = calculator.querySelectorAll('.btn');
+btn[0].onclick = addHandler;
+btn[1].onclick = subtractHandler;
+btn[2].onclick = multiplyHandler;
+btn[3].onclick = divideHandler;
 function addHandler() {
 	sign.innerHTML = '+';
 	resultOutput.innerHTML = +formerInput.value + +laterInput.value; // +为一元运算符，取正，没有明显的含义，但可强制转换为数字
