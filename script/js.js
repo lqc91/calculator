9d5a1cf54f790f167865724e76af2916b634c1fb
lqc91 <1007669205@qq.com>
2017-11-23 22:21:47 +0800

    4th modification: manage the code with objects

--- script/js.js
+++ script/js.js
@@ -1,10 +1,26 @@
-var calculator = document.querySelector('#calculator');
-var sign = calculator.querySelector('.sign');
-var formerInput = calculator.querySelector('.formerInput');
-var laterInput = calculator.querySelector('.laterInput');
-var resultOutput = calculator.querySelector('.resultOutput');
-var btn = calculator.querySelectorAll('.btn');
-each(btn, function (index, elem) {
+var wrapElem = document.querySelector('#calculator');
+var calculatorElem = {
+	sign: wrapElem.querySelector('.sign'),
+	formerInput: wrapElem.querySelector('.formerInput'),
+	laterInput: wrapElem.querySelector('.laterInput'),
+	resultOutput: wrapElem.querySelector('.resultOutput'),
+	btns: wrapElem.querySelectorAll('.btn')
+};
+var operation = {
+	add: function(num1, num2) {
+		return +num1 + +num2;
+	},
+	subtract: function(num1, num2) {
+		return num1 - num2;
+	},
+	multiply: function(num1, num2) {
+		return num1 * num2;
+	},
+	divide: function(num1, num2) {
+		return num1 / num2;
+	}
+};
+each(calculatorElem.btns, function (index, elem) {
 	elem.onclick = function() {
 		switch (this.title) {
 			case 'add':
@@ -29,35 +45,23 @@ function each(array, fn) {
 }
 function addHandler() {
 	updateSign('+');
-	outputResult(add(+formerInput.value, +laterInput.value)); // +为一元运算符，取正，没有明显的含义，但可强制转换为数字
+	outputResult(operation.add(+calculatorElem.formerInput.value, +calculatorElem.laterInput.value)); // +为一元运算符，取正，没有明显的含义，但可强制转换为数字
 }
 function subtractHandler() {
 	updateSign('-');
-	outputResult(subtract(formerInput.value, laterInput.value)); // 若两字符串相减，则自动转化为数字相减
+	outputResult(operation.subtract(calculatorElem.formerInput.value, calculatorElem.laterInput.value)); // 若两字符串相减，则自动转化为数字相减
 }
 function multiplyHandler() {
 	updateSign('*');
-	outputResult(multiply(formerInput.value, laterInput.value));
+	outputResult(operation.multiply(calculatorElem.formerInput.value, calculatorElem.laterInput.value));
 }
 function divideHandler() {
 	updateSign('/');
-	outputResult(divide(formerInput.value, laterInput.value));
+	outputResult(operation.divide(calculatorElem.formerInput.value, calculatorElem.laterInput.value));
 }
 function updateSign(symbol) {
-	sign.innerHTML = symbol;
-}
-function add(num1, num2) {
-	return +num1 + +num2;
-}
-function subtract(num1, num2) {
-	return num1 - num2;
-}
-function multiply(num1, num2) {
-	return num1 * num2;
-}
-function divide(num1, num2) {
-	return num1 / num2;
+	calculatorElem.sign.innerHTML = symbol;
 }
 function outputResult(fn) {
-	resultOutput.innerHTML = fn;
+	calculatorElem.resultOutput.innerHTML = fn;
 }
