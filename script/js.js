30f97a67e5a80019f8c3570f9f593d0f390290e6
lqc91 <1007669205@qq.com>
2017-11-24 00:08:34 +0800

    6th modification: modularization

--- script/js.js
+++ script/js.js
@@ -1,54 +1,62 @@
-var wrapElem = document.querySelector('#calculator');
-var calculatorElem = {
-	sign: wrapElem.querySelector('.sign'),
-	formerInput: wrapElem.querySelector('.formerInput'),
-	laterInput: wrapElem.querySelector('.laterInput'),
-	resultOutput: wrapElem.querySelector('.resultOutput'),
-	btns: wrapElem.querySelectorAll('.btn')
-};
-var operation = {
-	add: function(num1, num2) {
-		return +num1 + +num2;
-	},
-	subtract: function(num1, num2) {
-		return num1 - num2;
-	},
-	multiply: function(num1, num2) {
-		return num1 * num2;
-	},
-	divide: function(num1, num2) {
-		return num1 / num2;
-	},
-	addOperation: function(name, fn) {
-		if (!this[name]) {
-			this[name] = fn;
-		}
-		return this; // 链式调用
-	}
-};
-operation.addOperation('mod', function(num1, num2) {
-	return num1 % num2;
-}).addOperation('power', function(base, power) {
-	return Math.pow(base, power);
-});
-each(calculatorElem.btns, function (index, elem) {
-	elem.onclick = function() {
-		updateSign(this.value);
-		outputResult(operate(this.title, calculatorElem.formerInput.value, calculatorElem.laterInput.value));
+(function() {
+	var wrapElem = document.querySelector('#calculator');
+	var calculatorElem = {
+		sign: wrapElem.querySelector('.sign'),
+		formerInput: wrapElem.querySelector('.formerInput'),
+		laterInput: wrapElem.querySelector('.laterInput'),
+		resultOutput: wrapElem.querySelector('.resultOutput'),
+		btns: wrapElem.querySelectorAll('.btn')
 	};
-});
-function each(array, fn) {
-	for (var i = 0; i < array.length; i++) {
-		fn(i, array[i]);
+	var operate = (function() {
+		var operation = {
+			add: function(num1, num2) {
+				return +num1 + +num2;
+			},
+			subtract: function(num1, num2) {
+				return num1 - num2;
+			},
+			multiply: function(num1, num2) {
+				return num1 * num2;
+			},
+			divide: function(num1, num2) {
+				return num1 / num2;
+			},
+			addOperation: function(name, fn) {
+				if (!operation[name]) {
+					operation[name] = fn;
+				}
+				return operation; // 链式调用
+			}
+		};
+		function operate(name, num1, num2) {
+			if(!operation[name]) throw Error('不存在名为' + name + '的方法');
+			return operation[name].apply(operation, [].slice.call(arguments, 1, arguments.length));
+		}
+		operate.addOperation = operation.addOperation;
+		return operate;
+	})();
+	operate.addOperation('mod', function(num1, num2) {
+		return num1 % num2;
+	}).addOperation('power', function(base, power) {
+		return Math.pow(base, power);
+	}).addOperation('invert', function(num) {
+		return 1/num;
+	});
+	each(calculatorElem.btns, function (index, elem) {
+		elem.onclick = function() {
+			updateSign(this.value);
+			outputResult(operate(this.title, calculatorElem.formerInput.value, calculatorElem.laterInput.value));
+		};
+	});
+	function each(array, fn) {
+		for (var i = 0; i < array.length; i++) {
+			fn(i, array[i]);
+		}
 	}
-}
-function updateSign(symbol) {
-	calculatorElem.sign.innerHTML = symbol;
-}
-function outputResult(fn) {
-	calculatorElem.resultOutput.innerHTML = fn;
-}
-function operate(name, num1, num2) {
-	if(!operation[name]) throw Error('不存在名为' + name + '的方法');
-	return operation[name](num1, num2);
-}
+	function updateSign(symbol) {
+		calculatorElem.sign.innerHTML = symbol;
+	}
+	function outputResult(fn) {
+		calculatorElem.resultOutput.innerHTML = fn;
+	}
+})();
